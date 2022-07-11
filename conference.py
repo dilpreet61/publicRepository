fe043ff76cfc30ea6ec5e020787e268dd431c260
KatiSmith <107837618+KatiSmith@users.noreply.github.com>
2022-07-11 20:02:58 +0300

    Create conference.py
    Calculator for conference tickets

---
+++ conference.py
@@ -0,0 +1,31 @@
+print("Welcome to our Conference Hall!")
+
+# После приветствия пользователя просим ввести количество покупаемых билетов и возраст людей, которые преобразуются в листы с числовыми значениями;
+
+tickets = list(map(int, input("How many tickets do you want to buy?: ")))
+age = list(map(int, input("Please, enter the age of each person with space: ").split()))
+
+# Объявляем пустую переменную-счётчик и циклом for-in проходим по лист возрастов для определения стоимости билета;
+
+total_sum = 0
+for item in age:
+    if item < 18:
+        total_sum = 0
+    elif 18 < item < 25:
+        total_sum += 990
+    elif item > 25:
+        total_sum += 1390
+    else:
+        print("Sorry, something went wrong. Try again.")
+		
+# Для подсчёта 10-ти процентной скидки вводим новую переменную discount(скидка), которая вычитает процент и выводит на экран конечную сумму, 
+# пройдя по списку количества покупаемых билетов (tickets);
+per_cent = (total_sum / 100) * 10
+discount = total_sum - per_cent
+for item in tickets:
+    if item > 3:
+        print(f"Your total bill is {discount} rubles.")
+    elif item < 3:
+        print(f"Your total bill is {total_sum} rubles.")
+    else:
+        print("Sorry, something went wrong. Try again.")
